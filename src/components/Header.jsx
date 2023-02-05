1c586fc6cc589d76adfe00f6b9aee0c5e5462eb7
Baiel <hariswolendemord@gmail.com>
2023-02-05 12:01:56 +0600

    loading app

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <h1>Budget App <br />
+                <p class='BaIeL'>BaIeL</p>
+            </h1>
+        </div>
+    );
+};
+
+export default Header;
